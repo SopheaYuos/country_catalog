6ccc681eda12c235367e57a3b956063663495ad6
SopheaYuos <yuossophea2000@gmail.com>
2023-02-13 14:45:54 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+const counter = ref(0);
+
+function add() {
+  counter.value++;
+  console.log(counter);
+}
+const subtract = () => {
+  counter.value--;
+};
+</script>
+
+<template>
+  <div>
+    <div>Counter app</div>
+    <h2>{{ counter }}</h2>
+    <button @click="subtract">Substract</button>
+    <button @click="add">Add</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
