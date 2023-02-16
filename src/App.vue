d4b65e7914f93e679bdfdee9f6a45fb2bf5d15a7
SopheaYuos <yuossophea2000@gmail.com>
2023-02-16 13:33:53 +0700

    add modal component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
-
 import FuzzySearch from "fuzzy-search";
 import axios from "axios";
+import PopUp from "./components/PopUp.vue";
 export default {
   data() {
     return {
@@ -10,95 +10,132 @@ export default {
       search: "",
       results: "",
       noResults: "",
+      isOpen: false,
+      selectedObj: "",
+
     };
   },
-  computed: {
-   
+  components: {
+    PopUp,
   },
-  watch: {
-     
-  },
-
   mounted() {
     this.fetchData();
   },
   methods: {
-    fetchData: function() {
+    fetchData: function () {
       axios
         .get("https://restcountries.com/v3.1/all")
         .then((res) => {
           this.data = res.data;
+          
         })
         .catch((err) => {
           console.log(err, "err");
         });
     },
-     fuzzySearch: function() {
-      console.log("herer")
+    fuzzySearch: function () {
       if (this.search === "") return this.fetchData();
       const searcher = new FuzzySearch(this.data, ["name.official"]);
       const result = searcher.search(this.search);
       this.data = result;
-      console.log(result, "result");
       return result;
     },
+    sortAsc: function(prop,arr){
+        prop = prop.split('.');
+        var len = prop.length;
+        
+        arr.sort(function (a, b) {
+            var i = 0;
+            while( i < len ) {
+                a = a[prop[i]];
+                b = b[prop[i]];
+                i++;
+            }
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        return arr;
+    },
+    closeDialog: function(){
+      this.isOpen = false;
+    },
+    openModal(e){
+      this.selectedObj = this.data[e?.target?.id]
+      console.log(this.selectedObj, "e")
+      this.isOpen = true;
+    },
+     clickCallback: (pageNum) => {
+      console.log(pageNum)
+    }
+
   },
- 
 };
 </script>
 
 <template>
-  <div>
-      <section>
-          <label
-          for="default-search"
-          class="m-2 text-sm font-medium text-gray-100 sr-only text-white"
-          >Search</label
+  <div >
+   
+    <section class="flex direction-row justify-between ">
+      <div class="grow">
+      <label
+        for="default-search"
+        class="m-2 text-sm font-medium text-gray-100 sr-only text-white"
+        >Search</label
+      >
+      <div class="relative">
+        <div
+          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
-        <div class="relative">
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5 text-gray-500 dark:text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
-          </div>
-          <input
-            v-model="search"
-            @keyup="fuzzySearch"
-            id="default-search"
-            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none max-w-lg"
-            placeholder="Search Country Name..."
-            required
-          />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
         </div>
-      </section>
+        <input
+          v-model="search"
+          @keyup="fuzzySearch"
+          id="default-search"
+          class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none max-w-lg"
+          placeholder="Search Country Name..."
+          required
+        />
+        
+      </div>
+      
+    </div>
 
-
-    <div class="flex">
+    <div class="grow " >
+      
       <button
         class="m-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        @click="orderedUsers"
+        @click="sortAsc('name.official',data)"
       >
-        Sort ASC
+        ASC
       </button>
       <button
         class="m-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        @click="orderedUsers"
+        @click="sortAsc('name.official',data).reverse()"
       >
-        Sort DESC
+        DSC
       </button>
     </div>
+    </section>
     <span v-if="data.length == 0">
       <div
         class="flex p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 bg-gray-800 text-blue-400"
@@ -123,14 +160,20 @@ export default {
         </div>
       </div>
     </span>
-    <div class="flex justify-center inline-flex" v-for="d in data" :key="d.name.common">
+    <div
+      class="flex justify-center inline-flex"
+      v-for="(d,i) in data"
+      :key="i"
+    >
       <div class="ml-4 block p-6 rounded-lg shadow-lg bg-grey max-w-sm mr-2">
         <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
           <img class="h-16" :src="`${d.flags.png}`" alt="" />
         </h5>
-        <p class="text-gray-700 text-base w-44">Official: {{ d.name.official }}</p>
+        <p class="text-gray-700 text-base w-44">
+          Official: {{ d.name.official }}
+        </p>
         <p class="text-gray-700 text-base mb-4 w-44">
-          Native: {{ d.name.nativeName + d.languages?.official }}
+          Native: {{ d.name?.nativeName?.zho?.official || d.name.common }}
         </p>
         <p class="text-gray-700 text-base mb-4 w-44">
           Alternative Country Name: {{ d.altSpellings[0] }}
@@ -142,84 +185,21 @@ export default {
         <p>2 Country Code: {{ d.cca2 }}</p>
         <p>3 Country Code:{{ d.cca3 }}</p>
         <button
-          data-modal-target="popup-modal"
-          data-modal-toggle="popup-modal"
-          class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          type="button"
-        >
-          Toggle modal
-        </button>
-      </div>
-      <div
-        id="popup-modal"
-        tabindex="-1"
-        class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
-      >
-        <div class="relative w-full h-full max-w-md md:h-auto">
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button
+              @click="openModal($event)"
+              class="px-6 py-2 text-white bg-blue-600 rounded shadow"
               type="button"
-              class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-              data-modal-hide="popup-modal"
+              :id="i"
             >
-              <svg
-                aria-hidden="true"
-                class="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span class="sr-only">Close modal</span>
+              Show Details
             </button>
-            <div class="p-6 text-center">
-              <svg
-                aria-hidden="true"
-                class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
-              <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Are you sure you want to delete this product?
-              </h3>
-              <button
-                data-modal-hide="popup-modal"
-                type="button"
-                class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-              >
-                Yes, I'm sure
-              </button>
-              <button
-                data-modal-hide="popup-modal"
-                type="button"
-                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-              >
-                No, cancel
-              </button>
-            </div>
-          </div>
-        </div>
+        <PopUp :isOpen="isOpen" :selected-object="selectedObj"  @close-dialog="closeDialog" />
       </div>
     </div>
   </div>
 </template>
 
-<style >
-  html{
-    padding: 2rem;
-  }
+<style>
+html {
+  padding: 2rem;
+}
 </style>
---
+++ src/components/PopUp.vue
@@ -0,0 +1,100 @@
+
+
+
+<template>
+<div class="container mx-auto">
+    <div class="flex justify-center">
+    
+
+      <div
+        v-show="isOpen"
+        class="
+          fixed
+          inset-0
+          flex
+          items-center
+          justify-center
+          bg-gray-400 bg-opacity-5
+        "
+      >
+        <div class="max-w-2xl p-6 bg-white rounded-md">
+          <div class="flex items-center justify-between">
+            <h3 class="text-2xl">Model Title</h3>
+            <svg
+             @click="closeDialog"
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-8 h-8 text-red-900 cursor-pointer"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+          <div class="mt-4">
+            <p class="mb-4 text-sm">
+            id: {{rawData}}
+             <!-- Official Name: {{details?.name?.offical || 'null'}} -->
+            </p>
+            <p class="mb-4 text-sm">
+             <!-- Languages Name: {{details?.languages || 'sdfd'}} -->
+            </p>
+            <button
+              @click="closeDialog"
+              class="px-6 py-2 text-blue-800 border border-blue-600 rounded"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { isProxy, toRaw } from 'vue';
+    export default {
+      data(){
+        return{
+          rawData: {}
+        }
+      },
+      props:{
+        isOpen: {
+          type: Boolean,
+          default: false,
+        },
+        // details: Object,
+        selectedObj: Object
+        
+      },
+      methods: {
+        closeDialog(){
+          this.$emit("closeDialog");
+          
+        },
+        convertObj(){
+          if (isProxy(this.selectedObj)){ 
+          this.rawData = toRaw(this.selectedObj)
+          }
+        }
+        
+      },
+      computed() {
+        this.convertObj();
+      }
+
+
+    }
+  
+</script>
+
+
+
